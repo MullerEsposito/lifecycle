bc43247dd3d9ea790066a446b27c6851a7556909
Muller Esposito <mulleresposito@hotmail.com>
2021-03-04 01:34:39 -0300

    feat(App): project done;

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dogImage: '',
+      dogName: '',
+      loading: true
+    }
+  }
+
+  async componentDidMount() {
+    const myDog = localStorage.getItem('@DOGS:MyDogs');
+    if (!myDog) {
+      this.fetchDog();
+    } else {
+      const [{ dogName, dogImage }] = JSON.parse(myDog);
+      this.setState({dogName, dogImage, loading: false })
+    }
+  }
+  
+  fetchDog = async () => {
+    this.setState({ loading: true },
+      async () => { 
+        const { message } = await fetch('https://dog.ceo/api/breeds/image/random').then(r => r.json());
+        
+        if (message.includes('terrier')) return;
+
+        let dogBreed = message.match(/\/[A-Za-z-]+\/(?![A-Za-z-]+\/)/);
+        if (dogBreed) dogBreed = dogBreed[0].replaceAll('/','');   
+        dogBreed && alert(dogBreed);
+        
+        this.setState({ 
+          loading: false,
+          dogImage: message,
+          dogName: '',
+        })}
+        
+    );    
+  }
+
+  handleOnChange = ({ target: { name, value }}) => {
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleOnClickSaveDog = () => {
+    const { dogName, dogImage } = this.state;
+    const dogs = [{ dogName, dogImage }];
+
+    this.setState({dogName: ""},()=> {
+      localStorage.setItem('@DOGS:MyDogs', JSON.stringify(dogs));
+    })
+  }
+
+  render() {
+    const { dogImage, loading } = this.state;
+
+    return ( 
+      <div className="App">
+        <header className="App-header">
+          { loading 
+              ? <p>Loading...</p>
+              : <img src={dogImage} className="App-logo" alt="logo" /> 
+          }
+        <div style={{ marginTop: 100 }}>
+          <input name="dogName" value={this.state.dogName} onChange={this.handleOnChange} />
+          <button type="button" onClick={this.handleOnClickSaveDog}>Save dog</button>
+          <button  type="button" onClick={this.fetchDog}>Next</button>
+        </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
